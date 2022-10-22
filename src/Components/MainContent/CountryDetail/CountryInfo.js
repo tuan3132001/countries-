914bc038ceefca108e8076bc1d27fe19fac2ff93
Tuan <leanhtuan3103@gmail.com>
2022-10-22 15:53:37 +0700

    .

--- src/Components/MainContent/CountryDetail/CountryInfo.js
+++ src/Components/MainContent/CountryDetail/CountryInfo.js
@@ -1,8 +1,10 @@
 import styles from './countryInfoStyles.module.scss';
 import clsx from 'clsx';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../../ThemeContext/themeContext';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const getLanguages = (country) => {
     let result = '';
@@ -12,9 +14,28 @@ const getLanguages = (country) => {
     })
     return result;
 }
+
+const getCountryNameByCode = async (code) => {
+    const result = await axios.get(`https://restcountries.com/v2/alpha?codes=${code}`);
+    return result.data;
+}
+
 function CountryInfo(props) {
     const country = useSelector(state => state.Countries.country)
+    const [countriesBorder, setCountriesBorder] = useState([]);
     const themeContext = useContext(ThemeContext);
+
+    useEffect(() => {
+        if (country && country.borders) {
+            getCountryNameByCode(country.borders)
+                .then(res => {
+                    const countryName = res.map(country => country.name);
+                    setCountriesBorder(countryName);
+                })
+                .catch((err) => console.log(err))
+        }
+    }, [country])
+
     return (
 
         <div className={styles.countryInfo}>
@@ -73,9 +94,14 @@ function CountryInfo(props) {
                                     <td className={styles.countryInfo__title}>Border Countries</td>
                                     <td>:</td>
                                     <td className={styles.borderList}>
-                                        <div className={clsx(styles.borderItem, themeContext.theme)}>China</div>
-                                        <div className={clsx(styles.borderItem, themeContext.theme)}>India</div>
-                                        <div className={clsx(styles.borderItem, themeContext.theme)}>Border Countries</div>
+                                        {
+                                            countriesBorder.length > 0 && countriesBorder.map(country => (
+                                                <Link to={`/country/${country} `} key={country}>
+                                                    <div className={clsx(styles.borderItem, themeContext.theme)}>{country}</div>
+                                                </Link>
+                                            ))
+                                        }
+
                                     </td>
                                 </tr>
                             </tbody>
